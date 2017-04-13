7695d29ef1f5e92a05ab865dea77cd21a50a6dc6
Dana Kulp <dkulp23@gmail.com>
2017-04-13 13:04:33 -0700

    scaffolding views.

---
+++ app/config/router-config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
+
+function routerConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when('', '/join#signup');
+  $urlRouterProvider.when('/', '/join#signup');
+  $urlRouterProvider.when('/signup', '/join#signup');
+  $urlRouterProvider.when('/login', '/join#login');
+  $urlRouterProvider.when('/message', '/message');
+
+  let states = [
+    {
+      name: 'home',
+      url: '/home',
+      template: require('../view/home/home.html'),
+      controller: 'HomeController',
+      controllerAs: 'homeCtrl'
+    },
+    {
+      name: 'landing',
+      url: '/join',
+      template: require('../view/landing/landing.html'),
+      controller: 'LandingController',
+      controllerAs: 'landingCtrl'
+    },
+    {
+      name: 'message',
+      url: '/message',
+      template: require('../view/message/message.html'),
+      controller: 'messageController',
+      controllerAs: 'messageCtrl'
+    },
+    {
+      name: 'way',
+      url: '/way',
+      template: require('../view/way/way.html'),
+      controller: 'WayController',
+      controllerAs: 'wayCtrl'
+    }
+  ];
+
+  states.forEach( state => {
+    $stateProvider.state(state);
+  });
+}

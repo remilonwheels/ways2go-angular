88501de3620cd2115b440d426e8b0b8ee80980b7
Dana Kulp <dkulp23@gmail.com>
2017-04-15 20:14:22 -0700

    Refactored sign up into modal window.

---
+++ app/dialog/user/create-user/create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+
+require('./_create-user.scss');
+
+module.exports = {
+  template: require('./create-user.html'),
+  controller: ['$log', '$window', '$location', '$mdDialog', 'authService', 'items', SignupController],
+  controllerAs: 'signupCtrl'
+};
+
+function SignupController($log, $window, $location, $mdDialog, authService, items) {
+  $log.debug('SignupController');
+
+  this.isLoading = false;
+  this.user = {};
+  this.items = items;
+  $log.log(this.items);
+
+  if ($window.token) {
+    authService.getToken()
+    .then( () => {
+      $location.url('/home');
+    });
+  }
+
+  this.createUser = function() {
+    $log.debug('SignupController.signup');
+
+    this.isLoading = true;
+    authService.signup(this.user)
+    .then( () => {
+      this.isLoading = false;
+      // $location.url('/home');
+    })
+    .catch( err => {
+      $log.error('Signup Error', err);
+    });
+  };
+  this.closeDialog = function() {
+    $mdDialog.hide();
+  };
+}
--- app/view/landing/landing-controller.js
+++ app/view/landing/landing-controller.js
@@ -2,10 +2,29 @@
 
 require('./_landing.scss');
 
-module.exports = ['$log', '$location', '$rootScope', LandingController];
+const createUserComponent = require('../../dialog/user/create-user/create-user.js');
 
-function LandingController($log, $location) {
+module.exports = ['$log', '$rootScope', '$mdDialog', '$location', LandingController];
+
+function LandingController($log, $rootScope, $mdDialog, $location) {
   $log.debug('LandingController');
 
-  let url = $location.url() === '/join#signup' || url === '/join';
+  // let url = $location.url() === '/join#signup' || url === '/join';
+  this.signup = function($event) {
+    const parent = angular.element(document.body);
+    $mdDialog.show({
+      parent,
+      targetEvent: $event,
+      template: createUserComponent.template,
+      locals: {
+        items: this.items
+      },
+      controller: createUserComponent.controller,
+      controllerAs: createUserComponent.controllerAs
+    });
+  };
+
+  // $rootScope.$on('$locationChangeSuccess', () => {
+  //
+  // });
 }

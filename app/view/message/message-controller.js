9ae8e921f1169db154db0913447d373661d6dbe6
Anna Ulanova <anetteua0@gmail.com>
2017-04-19 11:27:43 -0700

    create message dialog and send message botton are done

--- app/view/message/message-controller.js
+++ app/view/message/message-controller.js
@@ -2,8 +2,27 @@
 
 require('./_message.scss');
 
-module.exports = ['$log', 'messageService', MessageController];
+const createMessageDialog = require('../../dialog/message/create-message/create-message.js');
 
-function MessageController($log, messageService) { // eslint-line-disable
+module.exports = ['$log', '$rootScope', '$mdDialog', 'messageService', '$http', '$interval', '$mdMedia', '$scope', MessageController];
+
+function MessageController($log, $rootScope, $mdDialog, messageService, $http, $interval, $mdMedia, $scope) { // eslint-line-disable
   $log.debug('MessageController');
+
+  this.createMessage = function ($event, bindFlag) {
+
+    //STANDARD DIALOG CONFIGURATION, YOU CAN ADD MORE
+    const dialogConfig = {
+      //makes dialog responsive
+      fullscreen: !$mdMedia('gt-sm'),
+      //makes dialog animate from the location of click
+      targetEvent: $event,
+      //binds data from the parent
+      scope: $scope.$new(bindFlag)
+    };
+
+    //DIALOG MAGIC!
+    $mdDialog.show(Object.assign(createMessageDialog, dialogConfig));
+
+  };
 }

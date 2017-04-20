71cc43ffd99341088dd078d8b432613385060f10
Remil Marzan <remilonwheels@gmail.com>
2017-04-19 17:48:00 -0700

    map markers are now linked to viewWay

--- app/component/way/waymap/waymap.js
+++ app/component/way/waymap/waymap.js
@@ -2,16 +2,18 @@
 
 require('./_waymap.scss');
 
+const viewWayComponent = require('../../../dialog/way/view-way/view-way.js');
+
 module.exports = {
   template: require('./waymap.html'),
-  controller: ['$log', '$http', '$interval', 'NgMap', 'wayService', WayMapController],
+  controller: ['$log', '$http', '$interval', 'NgMap', 'wayService', '$mdMedia', '$scope', '$mdDialog', WayMapController],
   controllerAs: 'wayMapCtrl',
   bindings: {
     ways: '<'
   }
 };
 
-function WayMapController($log, $http, $interval, NgMap, wayService) {
+function WayMapController($log, $http, $interval, NgMap, wayService, $mdMedia, $scope, $mdDialog) {
   $log.debug('WayMapController');
 
   //map config
@@ -30,18 +32,55 @@ function WayMapController($log, $http, $interval, NgMap, wayService) {
       this.paths = [];
 
       this.ways.forEach( way => {
-        this.startMarkers.push([way.startLocation.lat, way.startLocation.lng]);
+        let startMarker = new google.maps.Marker({
+          map: this.map,
+          position: new google.maps.LatLng(way.startLocation.lat, way.startLocation.lng),
+          wayID: way._id
+        });
 
-        this.endMarkers.push([way.endLocation.lat, way.endLocation.lng]);
+        let endMarker = new google.maps.Marker({
+          map: this.map,
+          position: new google.maps.LatLng(way.endLocation.lat, way.endLocation.lng),
+          wayID: way._id
+        });
 
-        this.paths.push([[way.startLocation.lat, way.startLocation.lng],[way.endLocation.lat, way.endLocation.lng]]);
+        this.startMarkers.push(startMarker);
+        this.endMarkers.push(endMarker);
+
+        google.maps.event.addListener(startMarker, 'click', function() {
+          let way = wayService.getOneWay(this.wayID);
+
+          viewWay(event, true, way);
+        });
+
+        google.maps.event.addListener(endMarker, 'click', function() {
+          console.log(this.wayID);
+
+          let way = wayService.getOneWay(this.wayID);
+
+          viewWay(event, true, way);
+        });
       });
 
-      // this.startMarkers = this.ways.map( way => [way.startLocation.lat, way.startLocation.lng]);
+      // METHOD THAT SHOWS MARKERS USING NG-REPEAT
+      // this.startMarkers = [];
+      // this.endMarkers = [];
+      // this.paths = [];
       //
-      // this.endMarkers = this.ways.map( way => [way.endLocation.lat, way.endLocation.lng]);
+      // this.ways.forEach( way => {
+      //   this.startMarkers.push([way.startLocation.lat, way.startLocation.lng]);
+      //
+      //   this.endMarkers.push([way.endLocation.lat, way.endLocation.lng]);
+      //
+      //   this.paths.push([[way.startLocation.lat, way.startLocation.lng],[way.endLocation.lat, way.endLocation.lng]]);
+      // });
 
+      console.log('map markers', map.markers);
     });
+  };
+
+  this.drawWays = function() {
+    //FINISH THIs
   };
 
   this.placeChanged = function() {
@@ -54,6 +93,25 @@ function WayMapController($log, $http, $interval, NgMap, wayService) {
     this.map.setCenter(this.place.geometry.location);
   };
 
+
+  this.wayClick = function(event) {
+    console.log('event in wayclick', event);
+    console.log('this in wayclick', this);
+  };
+
+  const viewWay = function ($event, bindFlag, way) {
+    const dialogConfig = {
+      fullscreen: !$mdMedia('gt-sm'),
+      targetEvent: $event,
+      scope: $scope.$new(bindFlag),
+      resolve: {
+        way: function() {
+          return way;
+        }
+      },
+    };
+    $mdDialog.show(Object.assign(viewWayComponent, dialogConfig));
+  };
 
   mapInit();
 }

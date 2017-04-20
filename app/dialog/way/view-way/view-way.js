33643e60c5cd92609df298aec6e69e2edff1d91e
Remil Marzan <remilonwheels@gmail.com>
2017-04-20 16:42:58 -0700

    initial way view form

--- app/dialog/way/view-way/view-way.js
+++ app/dialog/way/view-way/view-way.js
@@ -11,6 +11,8 @@ module.exports = {
 function ViewWayController($log, $mdDialog, $mdToast, wayService, way, $scope) {
 
   this.way = wayService.getOneWay(way._id);
+
+  console.log(this.way);
   this.name = this.way.name || 'Way';
 
   this.way.startLocation = way.startLocation.fullAddress ? way.startLocation.fullAddress : way.startLocation;
@@ -19,9 +21,29 @@ function ViewWayController($log, $mdDialog, $mdToast, wayService, way, $scope) {
 
   console.log('wayer 0', this.way.wayerz[0]);
 
-  this.daysOfWeek = ['M', 'T', 'W', 'R', 'F', 'Sa', 'Su'];
+  // this.daysOfWeek = ['M', 'T', 'W', 'R', 'F', 'Sa', 'Su'];
   this.isPM = true;
-  const dayMap = { M: 0, T: 1, W: 2, R: 3, F: 4, Sa: 5, Su: 6 };
+  // const dayMap = { M: 0, T: 1, W: 2, R: 3, F: 4, Sa: 5, Su: 6 };
+  const dayMap = { 0: 'Monday', 1:'Tuesday', 2:'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'};
+
+  this.daysOfWeekOutput = function() {
+    let dayArray = [];
+    let dayStr = '';
+
+    if (this.way.recurringDayOfWeek) {
+      this.way.recurringDayOfWeek.forEach( i => {
+        console.log(dayMap[i]);
+        dayArray.push(dayMap[i]);
+        dayStr.concat(`${dayMap[i]}, `);
+      });
+      return dayStr;
+
+    }
+
+    return;
+  }
+
+
 
   this.isLoading = false;
 

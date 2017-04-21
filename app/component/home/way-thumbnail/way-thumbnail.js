56bfb0cccdba68f470b81c5fb39c8daa7d9d3200
Remil Marzan <remilonwheels@gmail.com>
2017-04-21 08:48:49 -0700

    tried to add ng-repeat for home thimbnails, sometimes shows up sometimes not

--- app/component/home/way-thumbnail/way-thumbnail.js
+++ app/component/home/way-thumbnail/way-thumbnail.js
@@ -4,12 +4,24 @@ require('./_way-thumbnail.scss');
 
 module.exports = {
   template: require('./way-thumbnail.html'),
-  controller: ['$log', 'wayService', WayThumbnailController],
+  controller: ['$log', 'wayService','profileService', WayThumbnailController],
   controllerAs: 'waythumbnailCtrl'
 };
 
-function WayThumbnailController($log, wayService) { // eslint-line-disable
+function WayThumbnailController($log, wayService, profileService) { // eslint-line-disable
   $log.debug('WayThumbnailController');
+
+
+  profileService.fetchProfile()
+  .then( profile => {
+    // this.allWays = wayService.getWays();
+    this.profile = profile;
+    this.myWays = wayService.getWays().filter( way => {
+      way.profileID === this.profile._id;
+      console.log('w', way);
+    });
+    console.log('my ways', this.myWays);
+  })
 
   this.name = 'Cool way';
   this.startLocation = '123 code way';

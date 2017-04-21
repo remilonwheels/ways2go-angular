c3a93cf7788375bedfa4435972fca9a987cccb30
Anna Ulanova <anetteua0@gmail.com>
2017-04-20 17:18:14 -0700

    saving

--- app/dialog/way/view-way/view-way.js
+++ app/dialog/way/view-way/view-way.js
@@ -4,13 +4,15 @@ require('./_view-way.scss');
 
 module.exports = {
   template: require('./view-way.html'),
-  controller: ['$log', '$mdDialog', '$mdToast','wayService', 'way', '$scope', ViewWayController],
+  controller: ['$log', '$mdDialog', '$mdToast','wayService', 'way', '$scope', 'messageService', ViewWayController],
   controllerAs: 'viewWayCtrl'
 };
 
-function ViewWayController($log, $mdDialog, $mdToast, wayService, way, $scope) {
+function ViewWayController($log, $mdDialog, $mdToast, wayService, way, $scope, messageService) {
 
   this.way = wayService.getOneWay(way._id);
+
+  console.log(this.way);
   this.name = this.way.name || 'Way';
 
   this.way.startLocation = way.startLocation.fullAddress ? way.startLocation.fullAddress : way.startLocation;
@@ -19,28 +21,38 @@ function ViewWayController($log, $mdDialog, $mdToast, wayService, way, $scope) {
 
   console.log('wayer 0', this.way.wayerz[0]);
 
-  this.daysOfWeek = ['M', 'T', 'W', 'R', 'F', 'Sa', 'Su'];
+  // this.daysOfWeek = ['M', 'T', 'W', 'R', 'F', 'Sa', 'Su'];
   this.isPM = true;
-  const dayMap = { M: 0, T: 1, W: 2, R: 3, F: 4, Sa: 5, Su: 6 };
+  // const dayMap = { M: 0, T: 1, W: 2, R: 3, F: 4, Sa: 5, Su: 6 };
+  const dayMap = { 0: 'Monday', 1:'Tuesday', 2:'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'};
+
+  this.daysOfWeekOutput = function() {
+    let dayArray = [];
+    let dayStr = '';
+
+    if (this.way.recurringDayOfWeek) {
+      this.way.recurringDayOfWeek.forEach( i => {
+        console.log(dayMap[i]);
+        dayArray.push(dayMap[i]);
+        dayStr.concat(`${dayMap[i]}, `);
+      });
+      return dayStr;
+
+    }
+
+    return;
+  }
+
+
 
   this.isLoading = false;
 
-  this.editWaySubmit = function() {
+  this.joinSubmit = function() {
     this.isLoading = true;
 
-    if (this.hour12) {
-      this.way['startTime.hour'] = this.hour12;
-      if (this.ampm === 'pm') {
-        this.way['startTime.hour'] += 12;
-      }
-    }
 
-    this.way['startTime.minutes'] = this.way.startTime.minutes;
-    delete this.way.startTime;
 
-    console.log('this.way before api call', this.way);
-
-    wayService.editWay(this.way)
+    messageService.createMessage(this.way)
     .then( res => {
       console.log(res);
       $mdToast.showSimple('Changed Way Successfully');

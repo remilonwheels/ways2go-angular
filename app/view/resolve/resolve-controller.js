7d7184dc79973e65d1187a9829042830183b8f00
Remil Marzan <remilonwheels@gmail.com>
2017-04-24 22:50:20 -0700

    binded profile to viewway by removing parent scope

--- app/view/resolve/resolve-controller.js
+++ app/view/resolve/resolve-controller.js
@@ -3,6 +3,4 @@
 module.exports = ['$log', '$rootScope', '$mdDialog', 'wayService', '$http', '$interval', 'NgMap', '$mdMedia', '$scope', 'testresolve', 'myprofile', ResolveController];
 
 function ResolveController($log, $rootScope, $mdDialog, wayService, $http, $interval, NgMap, $mdMedia, $scope, testresolve, myprofile) {
-  console.log('testresolve',testresolve);
-  console.log('myprofile', myprofile);
 }

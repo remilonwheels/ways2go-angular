ce687ca527d775fa299d988535725d9d75e4a873
Remil Marzan <remilonwheels@gmail.com>
2017-05-05 11:34:46 -0700

    started my ways filter

--- app/view/way/way-controller.js
+++ app/view/way/way-controller.js
@@ -17,6 +17,7 @@ function WayController($log, $rootScope, $mdDialog, wayService, $http, $interval
   this.searchLocation = myProfile.address[0];
   this.searchRadius = 10;
   this.placeHolder = [];
+  // this.isShowMyWays = false;
   NgMap.getMap()
   .then( map => this.map = map);
 
@@ -28,6 +29,10 @@ function WayController($log, $rootScope, $mdDialog, wayService, $http, $interval
     this.distanceWays = this.ways
       .map( way => Object.assign(way, {distance: this.computeWayDistance(way) * meterToMile}))
       .filter( way => way.distance <= this.searchRadius);
+      // .filter( way => {
+      //   if (this.isShowMyWays) return way.profileID === this.profile._id;
+      //   else return true;
+      // });
 
     console.log('distance ways after calc', this.distanceWays);
     $scope.$broadcast('wayChange');

7785c8fcdd91ccaed70da0e61c175a4c4f7a2b28
Dana Kulp <dkulp23@gmail.com>
2017-05-08 16:42:51 -0700

    refactored toasts to show red or green close buttons based on success or error.

--- app/config/app-style-config.js
+++ app/config/app-style-config.js
@@ -22,7 +22,7 @@ function appStyleConfig($mdThemingProvider) {
       themeName: 'swag',
       primaryPalette: 'blue',
       accentPalette: 'green',
-      warnPalette: 'blue-grey',
+      warnPalette: 'red',
       backgroundPalette: 'grey'
     },
   };
